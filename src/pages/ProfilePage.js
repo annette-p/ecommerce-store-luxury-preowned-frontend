bbac069f55de691fe568d891235778ab6a973396
Annette Poh <coding.net.p@gmail.com>
2021-12-07 23:56:10 +0800

    add device responsiveness to profile navigation bar

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,6 @@
 import React, {useContext, useEffect} from 'react'
 import { BrowserRouter as Router, Switch, Route, Link, useHistory, useRouteMatch } from "react-router-dom";
+import Accordion from 'react-bootstrap/Accordion'
 // import UserProfileProvider from '../contexts/profile/UserProfileProvider';
 import MyPurchaseOrders from '../components/profile/MyPurchaseOrders'
 import MySellingOrders from '../components/profile/MySellingOrders'
@@ -21,45 +22,65 @@ export default function ProfilePage(){
         history.push("/");
     }
 
+    function renderSideNavigationBar() {
+        return (
+            <div className="ps-1 col-bg">
+                <div className="mt-3 col-hover">
+                    <div className="btn white-font mt-2">Dashboard</div>
+                </div>
+                <Link to={`${url}/purchases`}>
+                    <div className="mt-3 col-hover">
+                        <div className="btn white-font">My Purchases</div>
+                    </div>
+                </Link>
+                <Link to={`${url}/sellings`}> 
+                    <div className="mt-3 col-hover">
+                        <div className="btn white-font">My Selling</div>
+                    </div>
+                </Link>
+                <div className="mt-3 col-hover">
+                    <div className="btn white-font">Order History</div>
+                </div>
+                <Link to={`${url}/account-settings`}>
+                    <div className="mt-3 col-hover">
+                        <div className="btn white-font">Account Settings</div>
+                    </div>
+                </Link>
+                <div className="mt-3 col-hover">
+                    <div className="btn white-font mb-3" onClick={() => {logoutUser()}}>Logout</div>  
+                </div>
+            </div>
+        )
+    }
+
     return (
         <React.Fragment>
-            <div className="container">
+            <div className=""> {/* container */}
 
                 <Router>
 
                     <div className="row">
-                        {/* sidebar navigation list */}
-                        <div className="col-2">
-                            <div className="ps-1 col-bg">
-                                <div className="mt-3 col-hover">
-                                    <div className="btn white-font mt-2">Dashboard</div>
-                                </div>
-                                <Link to={`${url}/purchases`}>
-                                    <div className="mt-3 col-hover">
-                                        <div className="btn white-font">My Purchases</div>
-                                    </div>
-                                </Link>
-                                <Link to={`${url}/sellings`}> 
-                                    <div className="mt-3 col-hover">
-                                        <div className="btn white-font">My Selling</div>
-                                    </div>
-                                </Link>
-                                <div className="mt-3 col-hover">
-                                    <div className="btn white-font">Order History</div>
-                                </div>
-                                <Link to={`${url}/account-settings`}>
-                                    <div className="mt-3 col-hover">
-                                        <div className="btn white-font">Account Settings</div>
-                                    </div>
-                                </Link>
-                                <div className="mt-3 col-hover">
-                                    <div className="btn white-font mb-3" onClick={() => {logoutUser()}}>Logout</div>  
-                                </div>
-                            </div>
+
+                        {/* sidebar navigation list - for ipad and laptop */}
+                        <div className="d-none d-md-block col-md-4 col-lg-2">
+                            {renderSideNavigationBar()}
+                        </div>
+
+                        {/* sidebar navigation list - for mobile */}
+                        <div className="d-block d-md-none col-12 mt-4">
+                            <Accordion defaultActiveKey="0">
+                                <Accordion.Item eventKey="0">
+                                    <Accordion.Header>Navigation Menu</Accordion.Header>
+                                    <Accordion.Body>
+                                        {/* sidebar navigation list */}
+                                        {renderSideNavigationBar()}
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            </Accordion>
                         </div>
 
                         {/* Each profile content */}
-                        <div className="col-10">
+                        <div className="col-12 col-md-8 col-lg-10">
 
                             {/* <UserProfileProvider> */}
 
