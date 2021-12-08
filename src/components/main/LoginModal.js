27f96f4ca8d2eff7fa9b9ab765dc2d63eeb593c2
Annette Poh <coding.net.p@gmail.com>
2021-12-08 09:50:58 +0800

    set device responsiveness for login modal page

--- src/components/main/LoginModal.js
+++ src/components/main/LoginModal.js
@@ -5,6 +5,24 @@ import SignupForm from './SignupForm'
 
 export default function LoginModal({action}){
     const history = useHistory();
+
+    function renderLogoAndMotto() {
+        return (
+            <div>
+
+                <img src={require('../../images/logo/curve-lp-logo.jpg').default} className="img-fluid round-logo" alt="logo"/>
+                <div className="mt-4 motto-text fw-bold">
+                    Sustainable Luxury  
+                </div>
+                <div className="mt-1 motto-text">
+                    Give your wardrobe a second life 
+                </div>
+
+            </div>
+
+        )
+    }
+
     return (
         <React.Fragment>
             <div
@@ -15,21 +33,20 @@ export default function LoginModal({action}){
                     <div className="modal-content login-card">
                         {/* header */}
                         <div className="modal-header">
-                            <h4 className="modal-title">Welcome to Luxury Pre-owned</h4>
+                            <h4 className="d-none d-md-block modal-title ms-4">Welcome to Luxury Pre-owned</h4>
+                            <h5 className="d-block d-md-none modal-title text-center">Welcome to Luxury Pre-owned</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => {history.goBack()}}></button>
                         </div>
                         <div className="modal-body login-card">
                             <div className="container-fluid"> 
                                 <div className="row">
                                     {/* logo and motto column */}
-                                    <div className="col-md-6 vertical-line center-div">
-                                        <img src={require('../../images/logo/curve-lp-logo.jpg').default} className="img-fluid round-logo" alt="logo"/>
-                                        <div className="mt-4 motto-text fw-bold">
-                                            Sustainable Luxury  
-                                        </div>
-                                        <div className="mt-1 motto-text">
-                                            Give your wardrobe a second life 
-                                        </div>
+                                    <div className="d-none d-md-block col-md-6 vertical-line center-div">
+                                        {renderLogoAndMotto()}
+                                    </div>
+                                    {/* logo and motto column -- for mobile */}
+                                    <div className="d-block d-md-none col-md-6 center-div">
+                                        {renderLogoAndMotto()}
                                     </div>
                                     {action === "login" ? <LoginForm/> : null}
                                     {/* login column */}
