62802a33ae8421bd2193e807949f8620c3980077
Annette Poh <coding.net.p@gmail.com>
2021-12-11 14:41:45 +0000

    add logic to conditionally display EmptyCart component when cart is empty

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Offcanvas from 'react-bootstrap/Offcanvas'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons'
 import CartItems from '../components/cart/CartItems'
 import CartSummary from '../components/cart/CartSummary'
-// import EmptyCart from '../components/cart/EmptyCart'
+import EmptyCart from '../components/cart/EmptyCart'
+import CartContext from '../contexts/carts/CartContext'
 
 
 export default function CartPage({ ...props }){
+    
+    const [isCartEmpty, setIsCartEmpty] = useState(true);
+    const cartContext = useContext(CartContext);
+
+    useEffect(() => {
+        // retrieve the list of items in cart from CartContext, and check whether
+        // it is empty. Based on the number of items, set the value for "isCartEmpty" 
+        // state variable accordingly.
+        setIsCartEmpty(cartContext.getCartItems().length === 0);
+    }, [isCartEmpty, cartContext]);
+
     return (
         <React.Fragment>
             <Offcanvas show={props.show} onHide={props.handleClose} {...props}>   {/* is a short cut of: <Offcanvas onHide={props.handleClose} show={props.show} placement={props.placement}> 
@@ -19,14 +31,15 @@ export default function CartPage({ ...props }){
                 </Offcanvas.Header>
                 <Offcanvas.Body className="grey-bg white-font" id="cart-body"> 
                     <hr className="dark-grey cart-hr"></hr>
-                    {/* product item list */}
-                    <CartItems />
+                    
+                    {/* Display when the cart is empty */}
+                    {isCartEmpty ? <EmptyCart /> : null }
+                    
+                    {/* Display when cart is not empty - product item list */}
+                    {isCartEmpty ? null : <CartItems /> }
 
-                    {/* summary section + checkout buttons */}
-                    <CartSummary />
-
-                    {/* conditional rendering - when the cart is empty */}
-                    {/* <EmptyCart /> */}
+                    {/* Display when cart is not empty -  summary section + checkout buttons */}
+                    {isCartEmpty ? null : <CartSummary /> }
                      
                 </Offcanvas.Body>
             </Offcanvas>
