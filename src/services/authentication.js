388d46ba25cbc83dbd8f7bd121c1784f2ad3a992
Annette Poh <coding.net.p@gmail.com>
2021-12-11 14:23:13 +0000

    when user logout, call the remote api /logout to blacklist the user's refresh token

--- src/services/authentication.js
+++ src/services/authentication.js
@@ -56,22 +56,44 @@ export async function authenticateUser(username, password) {
     }
 }
 
-export function invalidateUserAuthentication() {
-    // remove the entry from local window storage
+// Perform required tasks in order to invalidate the session of the authenticated user
+export async function invalidateUserAuthentication() {
+
+    // Call the /users/logout api to blacklist the user's refresh token so that it 
+    // cannot be used by others in the event that this refresh token has been leaked.
+    try {
+        const refreshToken = getRefreshToken();
+        const headers = await generateHttpAuthzHeader(refreshToken);
+        const logoutInfo = {
+            refresh_token: refreshToken
+        }
+        await axios.post(`${global.apiUrl}/users/logout`, logoutInfo, headers);
+    } catch(err) {
+        console.log("Failed to perform remote logout to invalidate the user's refresh token. ", err)
+    }
+
+    // remove the information about the authenticated user from window local storage
     // ref: https://developer.mozilla.org/en-US/docs/Web/API/Storage/removeItem
     localStorage.removeItem("authenticatedUser")
 }
 
+// Retrieve the authenticated user's refresh token
 export function getRefreshToken() {
+
+    // the refresh token is stored in the window local storage after the user has been authenticated
     const user = JSON.parse(localStorage.getItem("authenticatedUser"));
     if (user) {
         return user.token;
     } else {
         return null;
     }
+
 }
 
+// Create a new user profile
 export async function createNewUserProfile(firstname, username, password, email) {
+
+    // the user profile type will be "Customer"
     let userInfo = {
         type: "Customer",
         firstname: firstname,
@@ -81,6 +103,8 @@ export async function createNewUserProfile(firstname, username, password, email)
     }
 
     try {
+
+        // call the api with the new user information to create the profile
         let signupResult = await axios.post(`${global.apiUrl}/users/create`, userInfo);
         /*
             A success user account sign-up response data from backend API will be:
@@ -106,7 +130,23 @@ export async function createNewUserProfile(firstname, username, password, email)
 
 // use refresh token to obtain a new access token
 export async function getAccessToken(refreshToken) {
+
+    /* Access Token
+    
+    When a user logins in, the server issues an access token, which the client applications 
+    can use to make secure calls to an API server. For security reasons, access tokens are
+    valid for short time. 
+
+    Refresh tokens which have longer valid time can be used to obtain a new access token
+    without the need for the user to constantly log in. 
+
+    ref: https://auth0.com/blog/refresh-tokens-what-are-they-and-when-to-use-them/
+
+    */
+
     if (refreshToken) {
+
+        // call api /users/refresh with refresh token to get a new access token
         let accessTokenResult = await axios.post(
             `${global.apiUrl}/users/refresh`, 
             {
@@ -125,9 +165,12 @@ export async function getAccessToken(refreshToken) {
             return null;
         }
     
+        // return the new access token
         let accessToken = accessTokenResult.data.accessToken;
         return accessToken;
+
     } else {
+        // refresh token does not exists.. likely the user has not log in
         return null
     }
     
@@ -135,6 +178,10 @@ export async function getAccessToken(refreshToken) {
 
 // generate http authorization header
 export async function generateHttpAuthzHeader(refreshToken) {
+
+    // Request for a new access token, and prepare the http authorization
+    // header
+    // ref: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
     const accessToken = await getAccessToken(refreshToken);
     if (accessToken !== null) {
         const headers = { 
@@ -148,7 +195,12 @@ export async function generateHttpAuthzHeader(refreshToken) {
     }
 }
 
+// Perform the password change for the user
 export async function changeUserPassword(currentPassword, newPassword) {
+
+    // Need 3 things - current password, new password, and access token
+    // access token is needed because the api /users/change-password is protected
+    // (i.e. need token to authenticate)
     try {
         const refreshToken = getRefreshToken();
         const headers = await generateHttpAuthzHeader(refreshToken);
